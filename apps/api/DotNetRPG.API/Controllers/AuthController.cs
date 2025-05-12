5683cd86e2e5a8695f595b001da71fb8bbbd25c7
IDEBeta21 <deladia.ian.bscs2018@gmail.com>
2025-05-13 00:32:36 +0800

    Initial Fluentvalidation implementation

--- apps/api/DotNetRPG.API/Controllers/AuthController.cs
+++ apps/api/DotNetRPG.API/Controllers/AuthController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using DotNetRPG.API.Data;
 using DotNetRPG.API.Dtos.User;
 using DotNetRPG.API.Models;
+using FluentValidation.Results;
 
 namespace DotNetRPG.API.Controllers
 {
@@ -33,11 +34,27 @@ namespace DotNetRPG.API.Controllers
         }
 
         [HttpPost("Login")]
-        public async Task<ActionResult<ServiceResponse<string>>> Login(UserRegisterDtoRequest request)
+        public async Task<ActionResult<ServiceResponse<string>>> Login(UserLoginDtoRequest request)
         {
-            var response = await _authRepo.Login(request.Username, request.Password);
-            if(!response.Success)
+            var validator = new UserLoginValidator();
+            ValidationResult results = validator.Validate(request);
+
+            if(!results.IsValid)
             {
+                var errors =  results.Errors
+                                .GroupBy(r => r.PropertyName.ToLower())
+                                .ToDictionary(
+                                    err => char.ToLowerInvariant(err.Key[0]) + err.Key.Substring(1),
+                                    err => err.First().ErrorMessage
+                                );
+
+                return BadRequest(errors);
+            }
+
+            var response = await _authRepo.Login(request.Username, request.Password);
+
+            if(!response.Success)
+            {   
                 return BadRequest(response);
             }
             return Ok(response);
--- apps/api/DotNetRPG.API/Dtos/User/UserLoginDtoRequest.cs
+++ apps/api/DotNetRPG.API/Dtos/User/UserLoginDtoRequest.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentValidation;
 
 namespace DotNetRPG.API.Dtos.User
 {
@@ -9,5 +11,17 @@ namespace DotNetRPG.API.Dtos.User
     {
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } =  string.Empty;
+    }
+
+    public class UserLoginValidator : AbstractValidator<UserLoginDtoRequest>
+    {
+        public UserLoginValidator()
+        {
+            RuleFor(user => user.Username)
+                .NotEmpty().WithSeverity(Severity.Warning).WithMessage("Username should not be null");
+
+            RuleFor(user => user.Password)
+                .NotEmpty().WithSeverity(Severity.Warning).WithMessage("Password should not be null");
+        }
     }
 }
