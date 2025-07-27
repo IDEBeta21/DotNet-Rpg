58d0a59dc7136e07be06d3783e2a73887590cec5
Ian Deladia (Ermino) <kitsmype416@gmail.com>
2025-07-28 00:57:04 +0800

    rename dtos

--- apps/api/DotNetRPG.API/Migrations/DataContextModelSnapshot.cs
+++ apps/api/DotNetRPG.API/Migrations/DataContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace DotNetRPG.API.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.3")
+                .HasAnnotation("ProductVersion", "8.0.8")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
